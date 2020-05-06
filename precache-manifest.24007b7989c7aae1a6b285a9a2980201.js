300dbe0a1c68560714dd1297829c0df783a5cbd5
Elite-Debashis <panda.debashis425@gmail.com>
2020-05-06 21:40:47 +0530

    Updates

---
+++ precache-manifest.24007b7989c7aae1a6b285a9a2980201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655a9c0a365509002e26ce91149a7217",
+    "url": "/contactmanager-react/index.html"
+  },
+  {
+    "revision": "542caa10de2a2decbda4",
+    "url": "/contactmanager-react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "84e39350d901398ad1c4",
+    "url": "/contactmanager-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "542caa10de2a2decbda4",
+    "url": "/contactmanager-react/static/js/2.4f9fa038.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager-react/static/js/2.4f9fa038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e39350d901398ad1c4",
+    "url": "/contactmanager-react/static/js/main.301403a9.chunk.js"
+  },
+  {
+    "revision": "6f0ef55387904389fa5e",
+    "url": "/contactmanager-react/static/js/runtime-main.16bb1a11.js"
+  }
+]);
